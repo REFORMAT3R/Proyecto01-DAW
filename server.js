d9e8b2fdea1b61a4bb084ba9dcce25650e55b574
Haku021 <jgonzalesra@unsa.edu.pe>
2026-05-05 23:15:35 -0500

    conversion de markdown a html

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
@@ -33,8 +34,21 @@ app.get("/api/files/:name", (req, res) => {
     res.json({
         html: `<h1>Contenido de ${nombre}</h1>`
     });
-
 });
+
+app.get('/md/:nombre', async(req, res)=>{
+    const { marked } = await import('marked');
+    const nombreArchivo= req.params.nombre;
+    const ruta = path.join(__dirname, 'markdowns', nombreArchivo + '.md')
+
+    fs.readFile(ruta, 'utf-8',(err,  data)=>{
+        if(err){
+            return res.send("Archivo no encontrado");
+        }
+        const html = marked(data);
+        res.send(html);
+    })
+})
 
 //API - recibir JSON
 
@@ -55,3 +69,4 @@ app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 
 });
+
